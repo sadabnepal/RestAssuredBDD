231f00a4822900143a16fc802e4b5902c771f24a
SadabSaqib <sadabnepal1993@gmail.com>
2021-01-27 22:49:30 +0530

    added surefire-plugin

--- src/test/java/com/testnepal/tests/GetRequestTest.java
+++ src/test/java/com/testnepal/tests/GetRequestTest.java
@@ -15,21 +15,25 @@ public final class GetRequestTest {
 
 	@Test
 	public static void getUsers() {	
+		System.out.println("Sending Request to fetch multiple user data...");
 		Response response = CRUDOperation.sendGETRequest("GET_LIST_OF_USERS");
 		response.then().assertThat().statusCode(200);
-
+		
 		JsonPath actualJsonResponse = JsonReaderUtil.rawToJson(response);
 		int allData = actualJsonResponse.getInt("data.size()");
 		for(int i=0; i<allData; i++) {
 			String email = actualJsonResponse.getString("data["+i+"].email").toLowerCase();
+			System.out.println("Fetching Email "+i+": "+email);
 			String fname = actualJsonResponse.getString("data["+i+"].first_name").toLowerCase();
 			String lname = actualJsonResponse.getString("data["+i+"].last_name").toLowerCase();
 			Assert.assertEquals(email, fname+"."+lname+"@reqres.in", "Validating Name with email ID");
 		}
+		System.out.println("All user data received...");
 	}
 	
 	@Test
 	public static void getSingleUser() {
+		System.out.println("Sending Request to fetch single user data...");
 		Response response  = CRUDOperation.sendGETRequest("GET_SINGLE_USER");
 		response.then().assertThat().statusCode(200);
 		
@@ -38,6 +42,7 @@ public final class GetRequestTest {
 		String fname = singleUserBody.getString("data.first_name").toLowerCase();
 		String lname = singleUserBody.getString("data.last_name").toLowerCase();
 		Assert.assertEquals(email, fname+"."+lname+"@reqres.in", "Validating Name with email ID");
+		System.out.println("Single user data fetched...");
 		
 	}
 }
